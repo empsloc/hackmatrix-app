a3a966494496a5578be6df5b10a1a2c29be87dc0
empsloc <empsloc@gmail.com>
2024-02-17 12:20:35 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/ThemeComponents/ThemeProvider/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><main><ThemeProvider>{children}</ThemeProvider></main></body>
     </html>
   );
 }
