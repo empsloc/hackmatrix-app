6172024bf7a47c887a3bc86025178a36f7b4e5ff
empsloc <empsloc@gmail.com>
2024-02-17 17:22:32 +0530

    ads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import MaxWidthWrapper from "@/components/helperComponents/MaxWidthWrapper/MaxWidthWrapper";
+import ClubsContainer from "@/components/homeComponents/clubsContainer/ClubsContainer";
+import CommunitiesContainer from "@/components/homeComponents/communitiesContainer/CommunitiesContainer";
+import HeadingContainer from "@/components/homeComponents/headingContainer/HeadingContainer";
+import Testimonials from "@/components/homeComponents/testimonials/Testimonials";
 
 export default function Home() {
   return (
+    
    <MaxWidthWrapper>
-    <div className=""></div>
+    <div className="mt-16">
+        <HeadingContainer/>
+    </div>
+    <div className="mt-24">
+      <CommunitiesContainer/>
+    </div>
+    <div className="mt-24">
+      <ClubsContainer/>
+    </div>
+
+    <div className="mt-24">
+      <Testimonials/>
+    </div>
+    
+    
    </MaxWidthWrapper>
   );
 }
